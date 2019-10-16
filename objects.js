5069c2de7cf218bfc33cd4be0b42c2cef90738f1
Marc BA <marc.bisbalarias@dowjones.com>
2019-10-16 10:17:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist={artistName: "Coldplay",songTitle: "Lost!"};
 
 function updatePlaylist(playlist,artistName,songTitle){
 
-  return Object.assign({},playlist,{artistName:["My Bloody Valentine"]},{songTitle:["Lost!"]});
+  return Object.assign({},playlist,{artistName:["Phil Ochs"]},{songTitle:["My Bloody Valentine","Slowdive"]});
 }
